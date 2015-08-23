dbc48763931b2c31f3bc1c1478005f0f6bbbc886
dibstigz <s-woods@hotmail.co.uk>
2015-08-23 15:23:34 +1000

    Blog Frame Image Update

--- blogFrame.php
+++ blogFrame.php
@@ -2,6 +2,6 @@
 $url = $_POST['url'];
 
 
-echo '<img style="height:100px;margin-bottom:20px;" src="http://ec2-54-79-18-12.ap-southeast-2.compute.amazonaws.com/wp-content/uploads/2015/07/logo.png" alt="" />';
+echo '<img style="height:100px;margin-bottom:20px;" src="http://ec2-54-79-18-12.ap-southeast-2.compute.amazonaws.com/wp-content/uploads/2015/08/logo.png" alt="" />';
 
 echo '<iframe height="100%" style="border:0px solid white;" id="yFrame" src="'.$url.'" width="100%"></iframe>';
